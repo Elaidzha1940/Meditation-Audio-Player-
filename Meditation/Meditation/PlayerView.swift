01115593822046654ec52c8efeb083370d0a069a
Elaidzha1940 <eli080293@gmail.com>
2023-02-12 21:28:33 +0800

    add a New File + PlayerView

---
+++ Meditation/Meditation/PlayerView.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerView.swift
+//  Meditation
+//
+//  Created by Elaidzha Shchukin on 12.02.2023.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        
+        ZStack {
+            Image("buddha")
+        }
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
