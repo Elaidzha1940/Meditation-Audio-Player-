cbb33049193636b1660f49995382c26b267b081b
Elaidzha1940 <eli080293@gmail.com>
2023-02-12 20:51:19 +0800

    Meditation App + Audio Player

---
+++ Meditation/Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Elaidzha Shchukin on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
