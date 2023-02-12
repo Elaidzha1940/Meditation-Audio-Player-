680c920642bf75906e8c09b7e464d0677ea76e40
Elaidzha1940 <eli080293@gmail.com>
2023-02-12 21:52:34 +0800

    add a New File

---
+++ Meditation/Meditation/PlaybackControlButton.swift
@@ -0,0 +1,20 @@
+//
+//  PlaybackControlButton.swift
+//  Meditation
+//
+//  Created by Elaidzha Shchukin on 12.02.2023.
+//
+
+import SwiftUI
+
+struct PlaybackControlButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaybackControlButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaybackControlButton()
+    }
+}
