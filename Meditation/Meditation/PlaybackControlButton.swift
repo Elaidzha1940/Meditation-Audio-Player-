527bc282c112220605545c5c2078fbdd75275f2e
Elaidzha1940 <eli080293@gmail.com>
2023-02-13 19:14:42 +0800

    add a New File

--- Meditation/Meditation/PlaybackControlButton.swift
+++ Meditation/Meditation/PlaybackControlButton.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct PlaybackControlButton: View {
+    
+    var systemName: String = "play"
+    var fontSize: CGFloat = 24
+    var color: Color = .white
+    var action: () -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button {
+            action()
+        } label: {
+            Image(systemName: systemName)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+        }
+
     }
 }
 
